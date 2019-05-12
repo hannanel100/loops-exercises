576fee18f8ec516223456f36672f9bdb98a44ed5
Hannanel Gershinsky <hannanel100@gmail.com>
2019-05-12 17:01:29 +0300

    trying to add when y is float - not succssesfull

--- js-ex/power.js
+++ js-ex/power.js
@@ -5,14 +5,20 @@ function powOfNum(num, pow) {
     }
     return result;
 }
-var num = -2;
-var pow = -3;
+function getLength(number) {// getting the length of a floating number so i could later respresent it as an integer multiplied by 10^-n where n is the return from this function
+    return (number.toString().length -1);
+}
+var num = 2;
+var pow = 0.525;
 var result = 1;
 if (pow < 0) {
     result = 1 / powOfNum(num, pow * (-1));
 }
-else if (pow > 0) {
+else if (pow >= 1) {
     result = powOfNum(num, pow);
+}
+else if (0 < pow && pow < 1) {
+    result =  getLength(pow);
 }
 console.log(result);
 
